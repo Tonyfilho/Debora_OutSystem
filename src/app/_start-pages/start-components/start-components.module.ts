a868ebfe27913ac9b15c589e1b02931f77269e7c
tonyfilho <oficinadosdrones@gmail.com>
2023-10-25 14:08:14 +0100

    fixing diretory

--- src/app/_start-pages/start-components/start-components.module.ts
+++ src/app/_start-pages/start-components/start-components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+import { AuthenticationComponent } from '../authentication/authentication.component';
 import { HeaderComponent } from './body/header/header.component';
 import { FooterComponent } from './body/footer/footer.component';
 import { BodyComponent } from './body/body.component';
-import { RouterModule } from '@angular/router';
-import { AuthenticationComponent } from './authentication/authentication.component';
 
 
 
